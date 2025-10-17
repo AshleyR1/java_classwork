3212b11031854301882f66c1dd78363da4584a81
AshleyR1 <ashleyr698@nycstudents.net>
2025-10-17 14:12:23 +0000

    a

--- Lesson-11-Classwork/Main.java
+++ Lesson-11-Classwork/Main.java
@@ -6,10 +6,22 @@ class Main {
   
   void print(Object o){ System.out.println(o);}
   void printt(Object o){ System.out.print(o);}
-
+   String getUsername(String email){
+      int pos=email.indexOf("@");
+      return 
   void init(){
-   
+    //problam1
+ 
+   //String  username= Input.readstring();
+    //getusername= username.indexOf("@");
+    //String userN= username.substring(getusername,0);
+    //System.out.println("username"+ userN);
 
+    }
+
+
+    //problam2
+    String split=Input.readString();
     
   }
 
