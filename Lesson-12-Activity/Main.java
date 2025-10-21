3e2cb7b11839b245d255040894d82f6b012a0f99
AshleyR1 <ashleyr698@nycstudents.net>
2025-10-21 14:07:54 +0000

    challeng1

---
+++ Lesson-12-Activity/Main.java
@@ -0,0 +1,42 @@
+class Main {
+
+  public static void main(String[] args) {
+    (new Main()).init();
+  }
+  void print(Object o){ System.out.println(o);}
+  void printt(Object o){ System.out.print(o);}
+// chalemgr 1
+
+ String creditCardType (String card){
+if(card.substring(0,1).equals("4")){
+        return "visa";
+      }else if(card.substring(0,2).equals("34")||card.substring(0,2).equals("37")){
+        return "America Express";
+      }else if (card.substring(0,2).equals("36")){
+      return"Diner Club";
+    }else if(card.substring(0,2).equals("51")||card.substring(0,2).equals("55")){
+      return "Masercard";
+    }else if(card.substring(0,4).equals("6011")||card.substring(0,2).equals("65")){
+      return "Discovery";
+    } else{
+      return "unknown";
+    }
+      
+
+
+
+    }
+    
+  
+  void init(){
+   /*String visa ="4";
+    String  America Express ="34 || 37";
+    String Diner Club ="36";
+   String Mastercard ="51 || 55";
+    String Discovery ="6011 || 65";*/
+   
+  }
+
+  
+  
+}
