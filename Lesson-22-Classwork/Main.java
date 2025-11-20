2645348e5715a28ef6e4fdad7da5649e353c719c
AshleyR1 <ashleyr698@nycstudents.net>
2025-11-20 15:28:31 +0000

    o

---
+++ Lesson-22-Classwork/Main.java
@@ -0,0 +1,33 @@
+class Main {
+  public static void main(String[] args) {
+    (new Main()).init();
+  }
+  void print(Object o){ System.out.println(o);}
+  void printt(Object o){ System.out.print(o);}
+
+  void init(){
+   String[] friends= {"firend1","friend2","friend3","friend4","friend5"};
+    double[] tempc={-4,5,0,1,2,3,4};
+    double[] tempf= convert(tempc);
+for(int i=0;i<tempf.length;i++){
+  tempf[i]= c[i] = c[i]*9/5+32;
+}
+  }
+
+double[] convert(double[] tc){
+double[] tc = new double[tempc.length];
+
+for(int i=0;i<f.length;i++){
+  f[i]= c[i] = c[i]*9/5+32;
+}
+return f;
+}
+String[] c = new String[friends.length];
+
+
+
+
+  double celsiusToFarhenheit(double c){
+        return c*9/5+32;
+  }
+}
