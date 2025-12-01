17242f7984a4181b56332b56c74d7fd4132a7441
AshleyR1 <ashleyr698@nycstudents.net>
2025-12-01 14:44:40 +0000

    y

---
+++ Lesson-24-Classwork/Main.java
@@ -0,0 +1,27 @@
+class Main {
+  public static void main(String[] args) {
+    (new Main()).init();
+  }
+  void print(Object o){ System.out.println(o);}
+  void printt(Object o){ System.out.print(o);}
+
+
+
+  void init(){
+    String bld +="";
+    for(int ascii=97;ascii<=122;ascii++){
+      int num=()
+    }
+
+  }
+
+
+
+   
+  int randInt(int lower, int upper){
+    int range = upper - lower;
+    return (int)(Math.random()*range+lower);
+  }
+
+ 
+}
