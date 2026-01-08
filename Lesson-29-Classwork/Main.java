195df830663096595d0e6d5c914663daa5ea7562
AshleyR1 <ashleyr698@nycstudents.net>
2026-01-08 14:53:53 +0000

    o

---
+++ Lesson-29-Classwork/Main.java
@@ -0,0 +1,33 @@
+class Main {
+  public static void main(String[] args) {
+    (new Main()).init();
+  }
+  void print(Object o){ System.out.println(o);}
+  void printt(Object o){ System.out.print(o);}
+/*1)
+Create a class of a dog using the UML in the Dog.PNG file.
+
+2)
+Every Class most have it's own file with the exact name as the class name with an extension .java
+
+3)
+In the init function, create 3 dogs: Toto, Snoopy, and Poophy. Make each dog bark.
+*\
+
+  void init(){
+
+    
+   
+    
+  }
+
+
+
+
+  
+  int randInt(int lower, int upper){
+    int range = upper - lower;
+    return (int)(Math.random()*range+lower);
+  }
+
+}
---
+++ Lesson-30-Activity/Lesson-30-Activity/Car.java
@@ -0,0 +1,9 @@
+class Car{
+
+
+}
+//construct
+car(){
+
+    
+}
