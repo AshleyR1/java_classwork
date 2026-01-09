950bdd273e7f8b8150ccb0be6cbff26f31c89bfd
AshleyR1 <ashleyr698@nycstudents.net>
2026-01-09 15:02:58 +0000

    y

--- Lesson-30-Activity/Lesson-30-Activity/Main.java
+++ Lesson-30-Activity/Lesson-30-Activity/Main.java
@@ -8,9 +8,19 @@ class Main {
   void printt(Object o){System.out.print(o);}
 
   void init(){
-car car1= new car("ford","black","Mustang",2025,31920);
-car car2= new car("jeep","white","2025 jeep wrangler",2025,32095);
-print(car1);
+Car car1= new Car("ford","black","Mustang",2025,31920);
+Car car2= new Car("jeep","white","2025 jeep wrangler",2025,32095);
+print("brand"+car1.brand);
+print("color"+car1.color);
+print("model"+car1.model);
+print("year"+car1.year);
+print("value"+car1.value);
+
+print("brand"+car2.brand);
+print("color"+car2.color);
+print("model"+car2.model);
+print("year"+car2.year);
+print("value"+car2.value);
   }
 
 
