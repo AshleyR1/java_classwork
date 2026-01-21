c5c5f58583d56cb913f88340e85dfdf407ba01e0
AshleyR1 <ashleyr698@nycstudents.net>
2026-01-21 15:16:02 +0000

    aaa

--- Lesson-33-Activity/Lesson-33-Activity/CartItem.java
+++ Lesson-33-Activity/Lesson-33-Activity/CartItem.java
@@ -5,13 +5,20 @@ class CartItem{
   boolean onSale;
 
   //Create the constructor
- 
+ CartItem( String itemName, double itemPrice, int quantity, boolean onSale,){
+  this.itemName = itemName;
+  this.itemPrice= itemPrice;
+  this.quantity = quantity;
+  this.onSale = onSale;
+ }
 
 
   
   // write getPrice based on condition detailed in 
   // challenges.txt
-
+double getItemPrice(){
+  for(int 1=0,i<itemPrice.lengnth){}
+} 
 
 
 }
